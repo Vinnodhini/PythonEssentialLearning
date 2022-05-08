4359bf7e68082c32da162428b080f77ee4277ea3
Vinnodhini <99971790+Vinnodhini@users.noreply.github.com>
2022-05-08 13:34:41 +0530

    py commit 1

---
+++ Hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Python Essential Training from LinkedIn
+
+# Basic program of all Print "Hello World!!"
+
+print('Hello World!!')
+
+print('Testing my first code !!')
+
+
+
+
