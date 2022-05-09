6be1111609fbbb1f739837f113094cbbe8db9bc3
Vinnodhini <99971790+Vinnodhini@users.noreply.github.com>
2022-05-09 09:57:18 +0530

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,25 @@
 #! usr/bin/env python3
 # Fibonacci series
 
-a = 0
-b = 1
+def main():
+    a = 0
+    b = 1
+    ran = get_input()
+    print_series(ran, a, b)
 
-num_range = int(input("please enter the range of the fibonacci series :"))
 
-for num in range(num_range):
-    c = a + b
-    print(b)
-    a = b
-    b = c
+def get_input():
+    num_range = int(input("please enter the range of the fibonacci series :"))
+    return num_range
 
+
+def print_series(num_range, a, b):
+    for num in range(num_range):
+        c = a + b
+        print(b)
+        a = b
+        b = c
+
+
+if __name__ == '__main__':
+    main()
